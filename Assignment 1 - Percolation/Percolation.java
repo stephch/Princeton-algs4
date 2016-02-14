9e2ba87a08a73d97cb97b5fac2426f4f89eaf7a1
stephch <chong.stephane@gmail.com>
2016-02-14 09:45:58 +0100

    Corrections

--- Assignment 1 - Percolation/Percolation.java
+++ Assignment 1 - Percolation/Percolation.java
@@ -2,62 +2,60 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
    private int size;
-   private WeightedQuickUnionUF nodes,nodes2;
+   private WeightedQuickUnionUF nodes, nodes2;
    private boolean[] openness;
-   
-   private int xyTo1D(int i, int j){
-       return (int) ((size*(i-1)+j)-1);
-   }
    
    public Percolation(int N) {
        if (N <= 0) throw new IllegalArgumentException("Illegal argument");
        size = N;
        nodes = new WeightedQuickUnionUF(size*size+2);
-       nodes2 = new WeightedQuickUnionUF(size*size);
+       nodes2 = new WeightedQuickUnionUF(size*size+1);
        openness = new boolean[size*size+2];
-       for (int i = 0; i < size; i++){
-           for (int j = 0; j < size; j++){
+       for (int i = 0; i < size; i++) {
+           for (int j = 0; j < size; j++) {
                openness[i*size+j] = false;
            }
        }
        openness[size*size] = true;
        openness[size*size+1] = true;
    }
-
+   
+   private int xyTo1D(int i, int j) {
+       return (int) (size*(i-1)+j-1);
+   }
+   
    public void open(int i, int j) {
        if (i <= 0 || i > size) throw new IndexOutOfBoundsException("row index i out of bounds");
        if (j <= 0 || j > size) throw new IndexOutOfBoundsException("row index j out of bounds");
        if (openness[xyTo1D(i, j)]) return;
        openness[xyTo1D(i, j)] = true;
-       // four different cases, if it is the first line then it is open
-       // if it is in the middle, you have to check if it is connected to the top
-       // if it is in the middle
+       
        if (i == 1) {
-           nodes.union(size*size,xyTo1D(i, j));
-           nodes2.union(size*size,xyTo1D(i, j));
+           nodes.union(size*size, xyTo1D(i, j));
+           nodes2.union(size*size, xyTo1D(i, j));
        }
-       if (i == size) nodes.union(size*size+1,xyTo1D(i, j));
-       if (i > 1){
-           if (openness[xyTo1D(i-1, j)]) {
+       if (i == size) nodes.union(size*size+1, xyTo1D(i, j));
+       if (i > 1) {
+           if (openness[xyTo1D(i-1, j)]) { //check the connection with the top
                nodes.union(xyTo1D(i, j), xyTo1D(i-1, j));
-               nodes2.union(xyTo1D(i, j), xyTo1D(i-1, j));//check the connection with the top
+               nodes2.union(xyTo1D(i, j), xyTo1D(i-1, j)); 
            }
        }
        if (i < size) {
-           if (openness[xyTo1D(i+1, j)]) {
-               nodes.union(xyTo1D(i, j), xyTo1D(i+1, j)); //check the connection with the bottom
+           if (openness[xyTo1D(i+1, j)]) { //check the connection with the bottom
+               nodes.union(xyTo1D(i, j), xyTo1D(i+1, j)); 
                nodes2.union(xyTo1D(i, j), xyTo1D(i+1, j));
            }
        }
        if (j > 1) {
-           if (openness[xyTo1D(i, j-1)]) {
-               nodes.union(xyTo1D(i, j), xyTo1D(i, j-1)); //check the connection with the left
+           if (openness[xyTo1D(i, j-1)]) { //check the connection with the left
+               nodes.union(xyTo1D(i, j), xyTo1D(i, j-1)); 
                nodes2.union(xyTo1D(i, j), xyTo1D(i, j-1));
            }
        }
-       if (j < size){
-           if (openness[xyTo1D(i, j+1)]) {
-               nodes.union(xyTo1D(i, j), xyTo1D(i, j+1)); //check the connection with the right
+       if (j < size) {
+           if (openness[xyTo1D(i, j+1)]) { //check the connection with the right
+               nodes.union(xyTo1D(i, j), xyTo1D(i, j+1)); 
                nodes2.union(xyTo1D(i, j), xyTo1D(i, j+1));
            }
        }
--- Assignment 1 - Percolation/PercolationStats.java
+++ Assignment 1 - Percolation/PercolationStats.java
@@ -6,7 +6,7 @@ public class PercolationStats {
     private double[] results;
     private int size;
     
-    public PercolationStats(int N, int T) {
+    public PercolationStats(int N, int T) { // perform T independent experiments on an N-by-N grid
         results = new double[T];
         if (N <= 0 || T <= 0) throw new java.lang.IllegalArgumentException();
         size = T;
@@ -16,7 +16,7 @@ public class PercolationStats {
             while (!perc.percolates()) {
                 int a = 1 + StdRandom.uniform(N);
                 int b = 1 + StdRandom.uniform(N);
-                while (perc.isOpen(a,b)) {
+                while (perc.isOpen(a, b)) {
                     a = 1 + StdRandom.uniform(N);
                     b = 1 + StdRandom.uniform(N);
                 }
@@ -26,20 +26,20 @@ public class PercolationStats {
             results[i] = 1.0 * count / (N*N);
         }
             
-    }// perform T independent experiments on an N-by-N grid
-    public double mean() {
+    }
+    public double mean() { // sample mean of percolation threshold
         return StdStats.mean(results);
-    }// sample mean of percolation threshold
-    public double stddev() {                    // sample standard deviation of percolation threshold
+    }
+    public double stddev() { // sample standard deviation of percolation threshold                   
         return StdStats.stddev(results);
     }
-    public double confidenceLo() {              // low  endpoint of 95% confidence interval
-        return (double) (mean()-1.96*stddev()/(double)(Math.sqrt(size)));
+    public double confidenceLo() { // low  endpoint of 95% confidence interval             
+        return (double)(mean()-1.96*stddev()/(double)(Math.sqrt(size)));
     }
-    public double confidenceHi() {              // high endpoint of 95% confidence interval
-        return (double) (mean()+1.96*stddev()/(double)(Math.sqrt(size)));
+    public double confidenceHi() { // high endpoint of 95% confidence interval             
+        return (double)(mean()+1.96*stddev()/(double)(Math.sqrt(size)));
     }
-    public static void main(String[] args) {    // test client (described below)
+    public static void main(String[] args) { // test client (described below)   
         int int1 = Integer.parseInt(args[0]);
         int int2 = Integer.parseInt(args[1]);
         PercolationStats perc = new PercolationStats(int1, int2);
